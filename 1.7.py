a742159ed987166560236a831039323fe5b8fda3
LUMBHA RAM <63182965+erlumbharam@users.noreply.github.com>
2020-04-26 21:48:55 +0530

    Create 1.7.py

---
+++ 1.7.py
@@ -0,0 +1,5 @@
+#EX-7-LUMBHA RAM
+#write a program to inser a string at the begining of every elements in a list
+a = [100,123,345,567,789,890,98,876,543,678]
+#enter customer before each element
+print(['customer{0}'.format(i) for i in a])
