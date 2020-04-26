f936d0ac05192fa29cfdeb20b5e467f761e5896e
LUMBHA RAM <63182965+erlumbharam@users.noreply.github.com>
2020-04-26 21:47:58 +0530

    Create 1.6.py

---
+++ 1.6.py
@@ -0,0 +1,6 @@
+#EX-6-LUMBHA RAM
+#Write a Python program to iterate over two lists simultaneously.
+num = [1, 2, 3]
+color = ['red', 'while', 'black']
+for (a,b) in zip(num, color):
+    print(a, b)
